3fb1ab5bd64c4a919a08c5815770c8a986057875
Wellington <wellington.urbano@opuspac.com.br>
2024-03-24 11:37:36 -0300

    Aula 06 - Usando o useState da forma certa

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,17 +1,29 @@
-import { useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
 
 export const Login = () => {
-const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-const handleClick = () =>{
-    navigate("/home");
-};
+    const handleEntrar = () => {
+        console.log(email);
+        console.log(password);
+    }
 
     return (
         <div>
-            <p>Login</p>
-            <button onClick={handleClick}>Home</button>
+            <form action="action">
+                <label>
+                    <span>Email</span>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+
+                <label>
+                    <span>Password</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+
+                <button onClick={handleEntrar} type="button">Entrar</button>
+            </form>
         </div>
     );
 }
